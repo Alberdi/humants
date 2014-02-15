0d04ae9f87f844fa0ef32db3a9b5da272856c19f
Alberdi <omenda@gmail.com>
2014-02-15 17:12:35 +0100

    Removes bad placed/bad designed tests.

--- test/test_entity.py
+++ test/test_entity.py
@@ -131,50 +131,5 @@ class TestEntityComponent(unittest.TestCase):
     self.e.update()
     self.assertTrue(self.e.attribute("updated"))
 
-class TestEntityCarry(unittest.TestCase):
-  def setUp(self):
-    self.e1 = entity.Entity()
-    self.e2 = entity.Entity()
-    self.e3 = entity.Entity()
-    self.e1.add_attribute("carrier")
-    self.e2.add_attribute("carrier")
-    self.e2.add_attribute("carriable")
-    self.e3.add_attribute("carriable")
-
-  def test_carry(self):
-    self.assertTrue(self.e1.carry(self.e2))
-    self.assertIn(self.e2, self.e1.carries())
-
-  def test_carry_entities(self):
-    self.e1.carry_entity(self.e2)
-    self.assertTrue(self.e1.carry(self.e3))
-    self.assertIn(self.e2, self.e1.carries())
-    self.assertIn(self.e3, self.e1.carries())
-
-  def test_carry_noncarrier(self):
-    self.assertFalse(self.e3.carry(self.e2))
-    self.assertNotIn(self.e2, self.e3.carries())
-    
-  def test_carry_noncarriable(self):
-    self.e2.remove_attribute("carriable")
-    self.assertFalse(self.e1.carry(self.e2))
-    self.assertNotIn(self.e2, self.e1.carries())
-
-  def test_carry_chain(self):
-    self.e2.carry(self.e3)
-    self.assertTrue(self.e1.carry(self.e2))
-    self.assertIn(self.e3, self.e2.carries())
-    self.assertIn(self.e2, self.e1.carries())
-    self.assertNotIn(self.e3, self.e1.carries())
-
-  def test_carry_not_self(self):
-    self.assertFalse(self.e2.carry(self.e2))
-
-  def test_carry_not_carried(self):
-    self.e1.carry(self.e3)
-    self.assertFalse(self.e2.carry(self.e3))
-    self.assertIn(self.e3, self.e1.carries())
-    self.assertNotIn(self.e3, self.e2.carries())
-
 if __name__ == '__main__':
   unittest.main()
