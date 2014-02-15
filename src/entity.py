fcb85f3bf75a3f9c38c111de563e45ee2515ce76
Alberdi <omenda@gmail.com>
2014-02-15 14:03:32 +0100

    Adds code to pass the message tests.

--- src/entity.py
+++ src/entity.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 class Entity:
   def __init__(self):
     self.attributes = defaultdict(lambda: None)
+    self.message_handlers = defaultdict(list)
 
   """ Attributes """
   def attribute(self, attribute):
@@ -17,3 +18,22 @@ class Entity:
       del self.attributes[attribute]
       return True
     return False
+
+  """ Messages """
+  def message(self, message):
+    for handler in self.message_handlers[message]:
+      handler(self)
+
+  def add_handler(self, message, function):
+    self.message_handlers[message].append(function)
+    return True
+
+  def remove_handler(self, message, function=None):
+    if message not in self.message_handlers:
+      return False
+    if function:
+      self.message_handlers[message].remove(function)
+    else:
+      del self.message_handlers[message]
+    return True
+      
