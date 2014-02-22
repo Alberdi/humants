a5f02336258ac820be254ce4a111be13d0f51158
Alberdi <omenda@gmail.com>
2014-02-22 12:13:16 +0100

    Removes useless commas.

--- test/test_components/test_mobile.py
+++ test/test_components/test_mobile.py
@@ -17,7 +17,7 @@ class TestMobile(unittest.TestCase):
     self.assertTrue(self.e.attribute("can_move"))
 
   def test_can_move_handler(self):
-    self.e.add_handler("can_move", lambda e,p,: e.add_attribute("test"))
+    self.e.add_handler("can_move", lambda e,p: e.add_attribute("test"))
     self.e.update()
     self.e.update()
     self.assertIsNone(self.e.attribute("test"))
@@ -28,7 +28,7 @@ class TestMobile(unittest.TestCase):
     for i in range(4):
       self.e.update()
     self.e.update_attribute("can_move", False)
-    self.e.add_handler("can_move", lambda e,p,: e.add_attribute("test"))
+    self.e.add_handler("can_move", lambda e,p: e.add_attribute("test"))
     self.e.update()
     self.assertFalse(self.e.attribute("test"))
     self.e.update()
