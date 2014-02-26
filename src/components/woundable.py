a9620a97fb34a20edaa4d768ad43120fcf0b1f02
Alberdi <omenda@gmail.com>
2014-02-26 20:16:15 +0100

    Adds woundable component.

---
+++ src/components/woundable.py
@@ -0,0 +1,23 @@
+import operator
+
+import component
+
+class Woundable(component.Component):
+  def __init__(self, max_wounds=10):
+    self.attributes = [("healable", True), ("woundable", True),
+                       ("wounds", 0), ("max_wounds", max_wounds)]
+    self.handlers = [("healed", self.healed_handler),
+                     ("wounded", self.wounded_handler)]
+
+  def healed_handler(self, e, p):
+    if e.attribute("healable"):
+      e.update_attribute("wounds", 1, lambda x,y: max(x-y, 0))
+      if e.attribute("wounds") == e.attribute("max_wounds")-1:
+        e.message("revived", {"reason": "heal"})
+
+  def wounded_handler(self, e, p):
+    if e.attribute("woundable"):
+      e.update_attribute("wounds", 1, operator.add)
+      if e.attribute("wounds") >=  e.attribute("max_wounds"):
+        e.message("died", {"reason": "wounds"})
+
---
+++ test/test_components/test_woundable.py
@@ -0,0 +1,44 @@
+import unittest
+
+import components.woundable
+import entity
+
+class TestWoundable(unittest.TestCase):
+  def setUp(self):
+    self.e = entity.Entity()
+    self.e.add_component(components.woundable.Woundable())
+
+  def test_healed(self):
+    self.e.update_attribute("wounds", 2)
+    self.e.message("healed", {"healer": entity.Entity()})
+    self.assertEqual(self.e.attribute("wounds"), 1)
+
+  def test_healed_unhealable(self):
+    self.e.update_attribute("wounds", 2)
+    self.e.update_attribute("healable", False)
+    self.e.message("healed", {"healer": entity.Entity()})
+    self.assertEqual(self.e.attribute("wounds"), 2)
+
+  def test_revived_message(self):
+    self.e.update_attribute("wounds", 10)
+    self.e.add_handler("revived", lambda e,p: e.add_attribute("test"))
+    self.e.message("healed", {"healer": entity.Entity()})
+    self.assertTrue(self.e.attribute("test"))
+
+  def test_wounded(self):
+    self.e.message("wounded", {"attacker": entity.Entity()})
+    self.assertEqual(self.e.attribute("wounds"), 1)
+
+  def test_wounded_unwoundable(self):
+    self.e.update_attribute("woundable", False)
+    self.e.message("wounded", {"attacker": entity.Entity()})
+    self.assertEqual(self.e.attribute("wounds"), 0)
+
+  def test_died_message(self):
+    self.e.add_handler("died", lambda e,p: e.add_attribute("test"))
+    self.e.update_attribute("max_wounds", 1)
+    self.e.message("wounded", {"attacker": entity.Entity()})
+    self.assertTrue(self.e.attribute("test"))
+
+if __name__ == "__main__":
+  unittest.main()
